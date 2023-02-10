d2a1cb19cfd637f8cf08278774e564ffc602d7e1
Victor Semin <seminva@sibur.ru>
2023-02-10 18:34:27 +0300

    decomposition webpack config

---
+++ config/build/buildWebpackConfig.ts
@@ -0,0 +1,25 @@
+import webpack from "webpack";
+import { buildLoaders } from "./buildLoaders";
+import { buildPlugins } from "./buildPlugins";
+import { buildResolvers } from "./buildResolvers";
+import { BuildOptions } from "./types/config";
+
+export function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
+const { paths, mode } = options;
+
+  return {
+    // entry: './src/index.ts',
+    mode: mode,
+    entry: paths.entry,
+    output: {
+      filename: '[name].[contenthash].bundle.js',
+      path: paths.build,
+      clean: true,
+    },
+    plugins: buildPlugins(options),
+    module: {
+      rules: buildLoaders(),
+    },
+    resolve: buildResolvers(),
+  }
+}
