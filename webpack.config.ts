5ad8348d05c51a563dd85462362baf736ff504ee
Victor Semin <seminva@sibur.ru>
2023-02-13 19:41:36 +0300

    add environment variables, scripts for dev and prod

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,7 +1,9 @@
 import path from 'path';
 import webpack from 'webpack';
 import { buildWebpackConfig } from './config/build/buildWebpackConfig';
-import { BuildPaths } from './config/build/types/config';
+import { BuildEnv, BuildPaths } from './config/build/types/config';
+
+export default (env: BuildEnv) => {
 
 const paths: BuildPaths = {
   entry: path.resolve(__dirname, 'src', 'index.ts'),
@@ -9,9 +11,9 @@ const paths: BuildPaths = {
   html: path.resolve(__dirname, "public", "index.html"),
 }
 
-const mode = 'development';
+const mode = env. mode || 'development';
+const PORT = env.port || 3000;
 const isDev = mode === 'development';
-const PORT = 3000;
 
 const config: webpack.Configuration = buildWebpackConfig({
   mode: 'development',
@@ -20,4 +22,5 @@ const config: webpack.Configuration = buildWebpackConfig({
   port: PORT,
 });
 
-export default config;
+return config
+}
