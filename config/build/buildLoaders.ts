d234914e447ad54d57d6d511cd514aab0a5d6584
Victor Semin <seminva@sibur.ru>
2023-02-14 14:13:39 +0300

    configure css-modules to isolate styles in dev-server and builds prod

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -2,7 +2,7 @@ import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import webpack from "webpack";
 import { BuildOptions } from "./types/config";
 
-export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
+export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
   // если не использует тайпскрипт, то нужен babel-loader
   // (перегоняет новый стандарт js в старый, чтобы всеми браузерами поддерживались)
   const typeScriptLoader = {
@@ -15,12 +15,17 @@ export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
     test: /\.s[ac]ss$/i,
     use: [
       // Creates `style` nodes from JS strings
-      options.isDev ? "style-loader" : MiniCssExtractPlugin.loader, 
+      isDev ? "style-loader" : MiniCssExtractPlugin.loader, 
       // Translates CSS into CommonJS
       {
         loader: "css-loader",
         options: {
-          modules: true,
+          modules: {
+            auto: (resourcePath: string) => Boolean(resourcePath.includes('.module.')),
+            localIdentName: isDev
+            ? "[path][name]__[local]--[hash:base64:8]"
+            : "[hash:base64:8]",
+          },
         },
       },
       // Compiles Sass to CSS
