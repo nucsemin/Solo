2b44335239b4c95a14f0dca862cf351f3459cdb2
Victor Semin <seminva@sibur.ru>
2023-02-20 16:02:50 +0300

    add simple theme switcher

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
-import "./index.scss";
+import "./styles/index.scss";
+
+export enum Theme {
+  LIGHT = 'light',
+  DARK = 'dark',
+}
 
 export function App() {
+  const [ theme, setTheme ] = useState<Theme>(Theme.LIGHT)
+  const toggleTheme = () => {
+    setTheme(theme === Theme.DARK ? Theme.LIGHT : Theme.DARK)
+  }
+
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>TOGGLE</button>
       <Link to={"/"}>Main Page</Link>
       <Link to={"/about"}>About Page</Link>
       <Suspense fallback={<div>Loading...</div>}>
