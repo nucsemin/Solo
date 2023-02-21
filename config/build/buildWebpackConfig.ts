dfaf67054584d0cbd4dd17989ddfb948941fd7f8
Victor Semin <seminva@sibur.ru>
2023-02-21 16:47:14 +0300

    fix paths absolutely errors, add mainFiles index

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -21,7 +21,7 @@ const { paths, mode, isDev } = options;
     module: {
       rules: buildLoaders(options),
     },
-    resolve: buildResolvers(),
+    resolve: buildResolvers(options),
     devtool: isDev ? 'inline-source-map' : undefined,
     devServer: isDev ? buildDevServer(options) : undefined,
   }
