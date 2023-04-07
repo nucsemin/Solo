700014b1622459767743ab069152e7eb0ed4cb71
Сёмин Виктор Александрович <seminva@sibur.ru>
2023-04-07 15:30:54 +0300

    add shared component AppLink

---
+++ src/shared/ui/AppLink/AppLink.tsx
@@ -0,0 +1,22 @@
+import { FC } from "react";
+import { Link, LinkProps } from "react-router-dom";
+import { classNames } from "shared/lib/classNames/classNames";
+import s from "./AppLink.module.scss";
+
+interface AppLinkProps extends LinkProps {
+  className?: string;
+}
+
+export const AppLink: FC<AppLinkProps> = (props) => {
+  const { to, className, children, ...otherProps } = props;
+
+  return (
+    <Link
+      to={to}
+      className={classNames(s.AppLink, {}, [className])}
+      {...otherProps}
+    >
+      {children}
+    </Link>
+  );
+};
