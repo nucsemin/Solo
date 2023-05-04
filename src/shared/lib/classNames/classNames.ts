c62f7129de6fc7b8816c36a30e369f1e435f8d80
Сёмин Виктор Александрович <seminva@sibur.ru>
2023-05-04 16:49:34 +0300

    fix pass test

--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -7,7 +7,7 @@ export function classNames(cls: string, mods: Mods = {}, additional: string[] = 
     return [
         cls,
         ...additional.filter(Boolean), // здесь могут прилетать undefinded, поэтому фильтрую по булеан типу
-        Object.entries(mods)
+        ...Object.entries(mods)
             .filter(([className, value]) => Boolean(value))
             .map(([className, value]) => className),
     ]
