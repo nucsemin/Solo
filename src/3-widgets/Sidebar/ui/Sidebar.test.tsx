2523cebe1e4b9093213ed2d64183c982296007b8
Сёмин Виктор Александрович <seminva@sibur.ru>
2023-06-06 16:19:20 +0300

    fix warning sidebar test

--- src/3-widgets/Sidebar/ui/Sidebar.test.tsx
+++ src/3-widgets/Sidebar/ui/Sidebar.test.tsx
@@ -1,9 +1,11 @@
 import { render, screen } from '@testing-library/react';
 import { Sidebar } from '3-widgets/Sidebar';
+import { withTranslation } from 'react-i18next';
 
 describe('Sidebar', () => {
     test('simple test', () => {
-        render(<Sidebar />);
+        const SidebarWIthTranslation = withTranslation()(Sidebar);
+        render(<SidebarWIthTranslation />);
         expect(screen.getByTestId('sidebar')).toBeInTheDocument(); // проверяет наличие в дом-дереве
         // screen.debug();
     });
