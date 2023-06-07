8d1a46e8b38424815d54eff2feb9a4852378ccd6
Сёмин Виктор Александрович <seminva@sibur.ru>
2023-06-07 23:22:21 +0300

    add css loader for storybook

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -1,5 +1,6 @@
 import path from 'path';
 import webpack from 'webpack';
+import { buildCssLoaders } from '../build/loaders/buildCssLoaders';
 import { BuildPaths } from '../build/types/config'; // используем относительный путь поскольку абсолютные работают только в папке src
 
 export default ({ config } : {config: webpack.Configuration}) => {
@@ -11,5 +12,6 @@ export default ({ config } : {config: webpack.Configuration}) => {
     };
     config.resolve?.modules?.push(paths.src);
     config.resolve?.extensions?.push('.ts', '.tsx');
+    config.module?.rules?.push(buildCssLoaders(true)); // true птмчт storybook будет использоваться только на этапе разработкиё
     return config;
 };
