8c869a5a697f1869a1183f249f467a7f5377b771
Сёмин Виктор Александрович <seminva@sibur.ru>
2023-06-19 21:27:52 +0300

    fix import path

--- src/2-pages/AboutPage/ui/AboutPage.stories.ts
+++ src/2-pages/AboutPage/ui/AboutPage.stories.ts
@@ -1,16 +1,16 @@
 import { Theme } from '1-app/providers/ThemeProvider';
-import { AboutPageAsync } from '2-pages/AboutPage/ui/AboutPage.async';
+import { AboutPage } from '2-pages/AboutPage';
 import ThemeDecorator from '6-shared/config/storybook/ThemeDecorator';
 import type { Meta, StoryObj } from '@storybook/react';
 
-const meta: Meta<typeof AboutPageAsync> = {
+const meta: Meta<typeof AboutPage> = {
     title: 'pages/AboutPage',
-    component: AboutPageAsync,
+    component: AboutPage,
     tags: ['autodocs'],
 };
 
 export default meta;
-type Story = StoryObj<typeof AboutPageAsync>;
+type Story = StoryObj<typeof AboutPage>;
 
 export const AboutPageAsyncLight: Story = {};
 
--- src/2-pages/MainPage/ui/MainPage.stories.ts
+++ src/2-pages/MainPage/ui/MainPage.stories.ts
@@ -1,16 +1,16 @@
 import { Theme } from '1-app/providers/ThemeProvider';
-import { MainPageAsync } from '2-pages/MainPage/ui/MainPage.async';
+import { MainPage } from '2-pages/MainPage';
 import ThemeDecorator from '6-shared/config/storybook/ThemeDecorator';
 import type { Meta, StoryObj } from '@storybook/react';
 
-const meta: Meta<typeof MainPageAsync> = {
+const meta: Meta<typeof MainPage> = {
     title: 'pages/MainPage',
-    component: MainPageAsync,
+    component: MainPage,
     tags: ['autodocs'],
 };
 
 export default meta;
-type Story = StoryObj<typeof MainPageAsync>;
+type Story = StoryObj<typeof MainPage>;
 
 export const MainPageAsyncLight: Story = {};
 
